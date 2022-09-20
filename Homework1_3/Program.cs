080abe7dc404fce0c63c9898d8bc03b64bfc819c
Sergey <serqey2020@outlook.com>
2022-09-20 16:35:27 +0300

    Task 3

---
+++ Homework1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите любое целое число");
+int user_number = int.Parse(Console.ReadLine());
+
+if (user_number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является не четным");
+}
