dac1b34a3c129421afb5ff8fb820813a375cd404
Sergey <serqey2020@outlook.com>
2022-09-20 11:37:08 +0300

    test 1

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -1,6 +1,29 @@
-﻿int number = 12;
+﻿using System;
+
+int number = 12;
 Console.WriteLine("Месяцев в году " + number);
 string n = number.ToString();
 Console.WriteLine("Введите ваше имя");
-string name = Console.ReadLine();
+string name = Console.ReadLine(); // Отдает все только строкой
 Console.WriteLine("Привет, " + name);
+Console.WriteLine("Сколько вам лет?");
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("Вам " + age + " лет");
+Console.WriteLine("Введите число а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(a + b);
+
+if(a > b)
+{
+    Console.WriteLine("Число а больше числа b");
+}
+else if(a < b)
+{
+    Console.WriteLine("Число b больше числа а");
+}
+else 
+{
+    Console.WriteLine("Число а равно числу b");
+}
