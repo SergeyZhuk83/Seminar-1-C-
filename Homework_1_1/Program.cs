2da4728060a40671235af9429be6a57454a7655b
Sergey <serqey2020@outlook.com>
2022-09-20 15:24:03 +0300

    Task 1

---
+++ Homework_1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два любых целых числа");
+int user_numberA = int.Parse(Console.ReadLine());
+int user_numberB = int.Parse(Console.ReadLine());
+
+if (user_numberA > user_numberB)
+{
+    Console.Write("max = user_namberA");
+}
+else
+{
+    Console.Write("max = user_namberB");
+}
