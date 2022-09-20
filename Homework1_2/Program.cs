9807385c79b534ccd0539bec1d5e573a2051cadd
Sergey <serqey2020@outlook.com>
2022-09-20 16:16:15 +0300

    Task 2

---
+++ Homework1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три любых целых числа");
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+int C = int.Parse(Console.ReadLine());
+int max = A;
+
+if (B > max)         
+{
+    max = B;
+}
+if (C > max)         
+{
+    max = C;
+}
+
+Console.WriteLine("max = " + max);
