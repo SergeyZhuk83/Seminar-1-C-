3cbeee4e4973f0a4e0e1aefdf7912fea97810e98
Sergey <serqey2020@outlook.com>
2022-09-24 13:21:39 +0300

    CreaterArrayNumber

---
+++ Test_3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+// int[] numbers = new int[10];
+// int[] a = {1, 14, 155, 12, 61, 26, 136, 13, 613, 56, 12};
+// Console.WriteLine(a[5]);
+// 1 - Что мы хотим получить .. (void - ничего, int - число, int[] - числовой массив)
+// 2 - Название функции ... (Любым)
+// 3 - Что мы хотим отдать ()
+
+int[] CreaterArrayNumber(int size)
+{
+    int[] array = new int[size];// Создаем массив задавая размер через size
+    for (int i = 0; i < size; i++)// Заполняем массив
+    {
+        array[i] = i+1;
+    }
+    return array;// Отдаем массив
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+int[] numbers = CreaterArrayNumber(20);
+WriteArray(numbers);
