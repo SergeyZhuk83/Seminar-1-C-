7671ca87187e39289601d8671b14528b964d6666
Sergey <serqey2020@outlook.com>
2022-09-24 11:27:59 +0300

    voit

---
+++ Test_2/Program.cs
@@ -0,0 +1,15 @@
+﻿void WriteTwo()
+{
+    Console.WriteLine(2);
+}
+void WriteHello(string name)
+{
+    Console.WriteLine("Hello, " + name);
+}
+string HelloTwoWord(string name)
+{
+    return "Hello, " + name;
+}
+WriteTwo();
+string word = HelloTwoWord("Sergey ");
+Console.WriteLine(word + "Как дела?");
