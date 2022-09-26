91823e077d6cf8943a895db950a5c6ec139b0411
Sergey <serqey2020@outlook.com>
2022-09-26 12:51:18 +0300

    seminar 3

---
+++ Test_4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+int NumberOne(int num, int NumberOfFirstNumbers)// Узнаем первые два числа
+{ 
+    while (num / Math.Pow(10,NumberOfFirstNumbers)  > 1)
+    {
+        num/=10;
+    }
+    return num;
+}
+
+int number = int.Parse(Console.ReadLine());
+if(number < 0 ) number *=-1;
+if(number > 99)
+{
+    int x = number % 100;
+    Console.WriteLine(NumberOne(number,2));// Указываем сколько первых цифр хотим получить
+}
+else if(number > 9)
+{
+    int x = number % 10;
+}
+else
+{
+    Console.WriteLine("Введите число больше 9");
+}
+// Найти полиндромное число можно с помощью: if(x/10 == y%10 && x%10 ==y/10)
