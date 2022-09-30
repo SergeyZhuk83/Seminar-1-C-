c7a82208cfcd4fa3d5c69fecd3fa8bbb068910ff
Sergey <serqey2020@outlook.com>
2022-09-30 10:45:55 +0300

    Task28

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// Нахождение Факториала.
+
+int ReadData(string msg)
+{
+    System.Console.Write(msg); 
+    int number = int.Parse(System.Console.ReadLine()??"0"); 
+    return number;
+}
+void PrintData(string msg, long value) 
+{
+    System.Console.WriteLine(msg + value);
+}
+
+long Faktor(int num)
+{
+    long res = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+int numN = ReadData("Введите число N: ");
+long outResult = Faktor(numN);
+PrintData("Факториал числа " + numN + " равен ", outResult);
